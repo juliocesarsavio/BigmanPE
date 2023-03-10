929cbb1348f071f43b7487025982ae5d3818a8b8
juliocesarsavio <96896702+juliocesarsavio@users.noreply.github.com>
2023-03-10 13:02:28 -0300

    d

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function sendLeadData() {
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", "854039dd-8144-4f30-955c-aaa69e3c9f8d");
+    myHeaders.append("Authorization", "8fd4ade8-a472-44ae-bf92-fa30c264b990");
     myHeaders.append("Content-Type", "application/json");
   
     var raw = JSON.stringify({
-      "authorization": "854039dd-8144-4f30-955c-aaa69e3c9f8d",
+      "authorization": "8fd4ade8-a472-44ae-bf92-fa30c264b990",
       "firstName": "Bala",
       "lastName": "dsadsa",
       "email": "juliocesarsaviofilho12312@gmail.com",
