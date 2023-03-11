a6ad0429af07ba50e5ef8e86dbc6d6fb0ab7b31d
juliocesarsavio <96896702+juliocesarsavio@users.noreply.github.com>
2023-03-11 18:08:30 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   var form = document.querySelector('form');
   form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    var email = form.querySelector('input[name="email"]').value; // Get the email value
-    // Make a POST request to the server with the email data
+    event.preventDefault(); // Impede o comportamento de envio padrão do formulário
+    var email = form.querySelector('input[name="email"]').value; // Obtém o valor do email
+    // Faz uma solicitação POST para o servidor com os dados do email e subscribed
     fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', {
       method: 'POST',
       body: JSON.stringify({ email: email, subscribed: true }),
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
         'Content-Type': 'application/json'
       }
     }).then(function(response) {
-      // Handle the response here
+      // Lida com a resposta aqui
       console.log(response);
     }).catch(function(error) {
-      // Handle the error here
+      // Lida com o erro aqui
       console.log(error);
     });
   });
