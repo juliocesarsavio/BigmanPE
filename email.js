5be805258b4819a874aa8776ef0897a1451684bf
juliocesarsavio <96896702+juliocesarsavio@users.noreply.github.com>
2023-03-11 17:56:10 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,44 +1,28 @@
-// encontre o formulário na página
-const form = document.querySelector('form');
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form[data-form-type="formoid"]');
 
-// adicione um ouvinte de eventos para o envio do formulário
-form.addEventListener('submit', (event) => {
-  // impedir que o formulário seja enviado diretamente
-  event.preventDefault();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  // obter o valor do campo de e-mail
-  const emailInput = document.querySelector('input[name="email"]');
-  const emailValue = emailInput.value;
+    const formData = new FormData();
+    const email = form.querySelector('input[name="email"]').value;
+    formData.append('email', email);
 
-  // criar um objeto de dados a ser enviado com o POST
-  const data = {
-    email: emailValue
-  };
-
-  // configurar a solicitação de POST
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
-  // fazer a solicitação de POST
-  fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', options)
+    fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', {
+      method: 'POST',
+      body: formData
+    })
     .then(response => {
-      // verificar se a resposta é bem-sucedida
       if (!response.ok) {
-        throw new Error('Ocorreu um erro ao enviar o formulário.');
+        throw new Error('Error: ' + response.status);
       }
 
-      // mostrar uma mensagem de sucesso ao usuário
-      const successAlert = document.querySelector('[data-form-alert]');
-      successAlert.style.display = 'block';
+      form.querySelector('[data-form-alert]').removeAttribute('hidden');
+      form.reset();
     })
     .catch(error => {
-      // mostrar uma mensagem de erro ao usuário
-      const errorAlert = document.querySelector('[data-form-alert-danger]');
-      errorAlert.style.display = 'block';
+      console.error(error);
+      form.querySelector('[data-form-alert-danger]').removeAttribute('hidden');
     });
+  });
 });
