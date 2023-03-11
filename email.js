ee59d84cd4bb99cc6ccd6cf0491de8b8ea562c20
juliocesarsavio <96896702+juliocesarsavio@users.noreply.github.com>
2023-03-11 17:51:50 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,19 +1,44 @@
-const form = document.getElementById('contact-form');
+// encontre o formulário na página
+const form = document.querySelector('form');
 
+// adicione um ouvinte de eventos para o envio do formulário
 form.addEventListener('submit', (event) => {
-event.preventDefault();
-const email = document.getElementById('email').value;
-const data = {
-email: email
-};
-fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', {
-method: 'POST',
-body: JSON.stringify(data),
-headers: {
-'Content-Type': 'application/json'
-}
-})
-.catch((error) => {
-console.error(error);
-});
+  // impedir que o formulário seja enviado diretamente
+  event.preventDefault();
+
+  // obter o valor do campo de e-mail
+  const emailInput = document.querySelector('input[name="email"]');
+  const emailValue = emailInput.value;
+
+  // criar um objeto de dados a ser enviado com o POST
+  const data = {
+    email: emailValue
+  };
+
+  // configurar a solicitação de POST
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  // fazer a solicitação de POST
+  fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', options)
+    .then(response => {
+      // verificar se a resposta é bem-sucedida
+      if (!response.ok) {
+        throw new Error('Ocorreu um erro ao enviar o formulário.');
+      }
+
+      // mostrar uma mensagem de sucesso ao usuário
+      const successAlert = document.querySelector('[data-form-alert]');
+      successAlert.style.display = 'block';
+    })
+    .catch(error => {
+      // mostrar uma mensagem de erro ao usuário
+      const errorAlert = document.querySelector('[data-form-alert-danger]');
+      errorAlert.style.display = 'block';
+    });
 });
