aafd20692cb25fbe8fce55be40121b13f1614bb1
juliocesarsavio <96896702+juliocesarsavio@users.noreply.github.com>
2023-03-11 17:58:31 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,28 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const form = document.querySelector('form[data-form-type="formoid"]');
-
+  var form = document.querySelector('form');
   form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData();
-    const email = form.querySelector('input[name="email"]').value;
-    formData.append('email', email);
-
+    event.preventDefault(); // Prevent the default form submission behavior
+    var email = form.querySelector('input[name="email"]').value; // Get the email value
+    // Make a POST request to the server with the email data
     fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', {
       method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error: ' + response.status);
+      body: JSON.stringify({ email: email }),
+      headers: {
+        'Content-Type': 'application/json'
       }
-
-      form.querySelector('[data-form-alert]').removeAttribute('hidden');
-      form.reset();
-    })
-    .catch(error => {
-      console.error(error);
-      form.querySelector('[data-form-alert-danger]').removeAttribute('hidden');
+    }).then(function(response) {
+      // Handle the response here
+      console.log(response);
+    }).catch(function(error) {
+      // Handle the error here
+      console.log(error);
     });
   });
 });
