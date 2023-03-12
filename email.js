dadd2e6a37f142996955319f5406329abfb6bc5d
juliocesarsavio <96896702+juliocesarsavio@users.noreply.github.com>
2023-03-12 13:43:36 -0300

    de

--- email.js
+++ email.js
@@ -1,21 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var form = document.querySelector('form');
-  form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o comportamento de envio padrão do formulário
-    var email = form.querySelector('input[name="email"]').value; // Obtém o valor do email
-    // Faz uma solicitação POST para o servidor com os dados do email e subscribed
-    fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', {
-      method: 'POST',
-      body: JSON.stringify({ email: email, subscribed: true }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(function(response) {
-      // Lida com a resposta aqui
-      console.log(response);
-    }).catch(function(error) {
-      // Lida com o erro aqui
-      console.log(error);
-    });
-  });
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+alert("funcionou");
+const email = document.getElementById('email').value;
+const data = {
+email: email
+};
+fetch('https://h6gve5l5ajp4xvv6e7cmxjbepa0okysb.lambda-url.sa-east-1.on.aws/', {
+method: 'POST',
+body: JSON.stringify(data),
+headers: {
+'Content-Type': 'application/json'
+}
+})
+.catch((error) => {
+console.error(error);
+});
 });
